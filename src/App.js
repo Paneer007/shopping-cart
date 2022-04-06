5c8e0f347b946269e00882ad30d603f79971a633
Sanjai Kumar <sanjaikumar.mani@gmail.com>
2022-04-06 21:15:08 +0530

    Added more stuff

--- src/App.js
+++ src/App.js
@@ -5,31 +5,43 @@ import Catalog from './components/Catalog';
 import Checkout from './components/Checkout';
 import { useEffect, useState } from 'react';
 import CatalogItemView from './components/CatalogItemView';
+const DisplayResult=({imageData,setSearchFilter})=>{
+    return(
+      <div>
+        <p>{imageData.user.name}</p>
+        <Link onCLick={()=>setSearchFilter('')} to={`/catalog/${imageData.id}`}>Link To Page</Link>
+      </div>
+    )
+}
 function App() {
   const RandomPrice =()=> Math.floor(Math.random() * 9000) + 1000
   const [imageList,setImageList]=useState([])
   const [userShoppingCart,setUserShoppingCart]=useState({})
   const [countOfItem,setCountOfItem]=useState(0)
+  const [searchFilter,setSearchFilter]=useState(null)
   useEffect(()=>{
     const fetchData =async()=>{
         const response1 = await axios.get('https://api.unsplash.com/search/photos/?client_id=gH-fpaQSEHTHvDjtKznfrbkVetOrxR8YkWqEt02uxK0&query=art&per_page=50')
         let finalSetOfImages=[...response1.data.results]
         finalSetOfImages=finalSetOfImages.map(x=>Object.assign(x,{price:RandomPrice()}))
-        console.log(finalSetOfImages)
         setImageList(finalSetOfImages)
-        console.log('f')
     }
     fetchData()
   },[])
-  useEffect(()=>{
-    setCountOfItem(userShoppingCart.length===undefined?0:userShoppingCart.length)
-  },[userShoppingCart])
+  const searchResult=imageList.filter(x=>x.user.name.includes(searchFilter))
+  console.log(searchResult)
   return (
     <div className="App">
       <Router>
         <div>
           <Link to='/home'>Home</Link>
-          <input placeholder="Enter something to search"></input>
+          <div>
+            <input onChange={(e)=>setSearchFilter(e.target.value)} placeholder="Enter something to search"></input>
+            <div className={`${searchResult.length===0?'hidden':'visible'}`} id='searchFilter'>
+              {imageList.map(x=>x.user.name.includes(searchFilter)&&searchFilter!==''?<DisplayResult setSearchFilter={setSearchFilter} imageData={x}/>:null)}
+            </div>
+          </div>
+
           <div> 
             <Link to='/catalog'>Catalog</Link>
             <Link to='/cart' >Cart-{countOfItem}</Link>  
@@ -41,7 +53,7 @@ function App() {
           <Route path="/home" element={<Home imageList={imageList} />}/>
           <Route path='/cart' element={<Checkout item={imageList} userItem={userShoppingCart}/>}/>
           <Route path='/catalog' element={<Catalog imageList={imageList} userCart={userShoppingCart} setUserCart={setUserShoppingCart}/>}/>
-          {imageList.length===0?console.log('wait'):imageList.map(x=><Route path={`/catalog/${x.id}`} element={<CatalogItemView item={x} userCart={userShoppingCart} setUserCart={setUserShoppingCart}/>}/>)}
+          {imageList.length===0?null:imageList.map(x=><Route path={`/catalog/${x.id}`} element={<CatalogItemView item={x} userCart={userShoppingCart} setUserCart={setUserShoppingCart}/>}/>)}
         </Routes>
         
       </Router>
--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -25,6 +25,7 @@ const CatalogCard =({object,userCart,setUserCart})=>{
         <div className="h-96 w-96 mt-8 mb-8 flex flex-col ">
             <img className="h-72 w-72"src={object.urls.small}/>
             <div>
+                <p>Made by-{object.user.name}</p>
                 <p>Price: {object.price}</p>
                 {userCart[object.id] === undefined?<p onClick={addItemToCart}>Add</p>:(<input type="Number" min="1" className="border-2 border-black-700" value={userCart[object.id]} onChange={addItemToCart}></input>)}
                 <Link to={`/catalog/${object.id}`}>Learn More</Link>
